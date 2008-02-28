ed72f3ddbcc4d2f965d131176aee0ab8d8c9dde7
stu <stu@fe6a39be-99a9-dc11-9a70-001c23e17821>
2008-02-28 16:37:09 +0000

    turn off parts of xss_shield that are blowing up Fizmo

--- vendor/xss-shield/lib/xss_shield.rb
+++ vendor/xss-shield/lib/xss_shield.rb
@@ -1,5 +1,6 @@
 require 'xss_shield/safe_string'
 # Tarantula doesn't use haml
 # require 'xss_shield/haml_hacks'
-require 'xss_shield/erb_hacks'
+# ERB hacks blow up Rails
+# require 'xss_shield/erb_hacks'
 require 'xss_shield/secure_helpers'
--- vendor/xss-shield/lib/xss_shield/erb_hacks.rb
+++ vendor/xss-shield/lib/xss_shield/erb_hacks.rb
@@ -6,56 +6,56 @@ class XSSProtectedERB < ERB
       content = ''
       scanner = make_scanner(s)
       scanner.scan do |token|
-	if scanner.stag.nil?
-	  case token
+  if scanner.stag.nil?
+    case token
           when PercentLine
-	    out.push("#{@put_cmd} #{content.dump}") if content.size > 0
-	    content = ''
+      out.push("#{@put_cmd} #{content.dump}") if content.size > 0
+      content = ''
             out.push(token.to_s)
             out.cr
-	  when :cr
-	    out.cr
-	  when '<%', '<%=', '<%#'
-	    scanner.stag = token
-	    out.push("#{@put_cmd} #{content.dump}") if content.size > 0
-	    content = ''
-	  when "\n"
-	    content << "\n"
-	    out.push("#{@put_cmd} #{content.dump}")
-	    out.cr
-	    content = ''
-	  when '<%%'
-	    content << '<%'
-	  else
-	    content << token
-	  end
-	else
-	  case token
-	  when '%>'
-	    case scanner.stag
-	    when '<%'
-	      if content[-1] == ?\n
-		content.chop!
-		out.push(content)
-		out.cr
-	      else
-		out.push(content)
-	      end
-	    when '<%='
+    when :cr
+      out.cr
+    when '<%', '<%=', '<%#'
+      scanner.stag = token
+      out.push("#{@put_cmd} #{content.dump}") if content.size > 0
+      content = ''
+    when "\n"
+      content << "\n"
+      out.push("#{@put_cmd} #{content.dump}")
+      out.cr
+      content = ''
+    when '<%%'
+      content << '<%'
+    else
+      content << token
+    end
+  else
+    case token
+    when '%>'
+      case scanner.stag
+      when '<%'
+        if content[-1] == ?\n
+    content.chop!
+    out.push(content)
+    out.cr
+        else
+    out.push(content)
+        end
+      when '<%='
               # NOTE: Changed lines
-	      out.push("#{@insert_cmd}((#{content}).to_s_xss_protected)")
+        out.push("#{@insert_cmd}((#{content}).to_s_xss_protected)")
               # NOTE: End changed lines
-	    when '<%#'
-	      # out.push("# #{content.dump}")
-	    end
-	    scanner.stag = nil
-	    content = ''
-	  when '%%>'
-	    content << '%>'
-	  else
-	    content << token
-	  end
-	end
+      when '<%#'
+        # out.push("# #{content.dump}")
+      end
+      scanner.stag = nil
+      content = ''
+    when '%%>'
+      content << '%>'
+    else
+      content << token
+    end
+  end
       end
       out.push("#{@put_cmd} #{content.dump}") if content.size > 0
       out.close
