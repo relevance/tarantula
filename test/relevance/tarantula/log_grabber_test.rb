4864481583c5a144e83cd6bca27b73a4e327bdd0
Aaron J. Bedra <aaron@thinkrelevance.com>
2008-05-29 15:18:22 -0700

    adding log file creation

--- test/relevance/tarantula/log_grabber_test.rb
+++ test/relevance/tarantula/log_grabber_test.rb
@@ -2,23 +2,26 @@ require File.join(File.dirname(__FILE__), "..", "..", "test_helper.rb")
 include Relevance::Tarantula
 
 describe 'Relevance::Tarantula::LogGrabber' do
-  before {@grabber = LogGrabber.new(log_file)}
+  before do
+    @grabber = LogGrabber.new(log_file)
+    File.new(log_file)
+  end
 
   def log_file
     File.join(File.join(test_output_dir, "example.log"))
   end
-  
+
   it "can clear the log file" do
     File.open(log_file, "w") {|f| f.print "sample log"}
-    File.size(log_file).should == 10  
+    File.size(log_file).should == 10
     @grabber.clear!
-    File.size(log_file).should == 0  
+    File.size(log_file).should == 0
   end
-  
+
   it "can grab the log file" do
     File.open(log_file, "w") {|f| f.print "sample log"}
     @grabber.grab!.should == "sample log"
     File.size(log_file).should == 0
   end
-  
+
 end
