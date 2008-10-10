901ad60bd7e292fae05ef7535c1ebca7f930b327
Lawrence Karnowski <larry@thinkrelevance.com>
2008-10-10 11:33:22 -0400

    Commenting out running-as-a-gem because it's not working and breaking running-as-a-plugin.

--- init.rb
+++ init.rb
@@ -1,1 +1,2 @@
-require File.dirname(__FILE__) + "/rails/init"
+# Commenting out until running-as-a-gem works.  (This was breaking running-as-a-plugin.) -- LJK 2008-10-10
+#require File.dirname(__FILE__) + "/rails/init"
