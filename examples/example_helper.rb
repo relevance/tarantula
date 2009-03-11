8032bbf0ff48849f61ab09534e54ea2d5393fd01
Rob Sanheim <rsanheim@gmail.com>
2009-03-11 02:28:26 -0400

    add link to RCR and dont use doc formatter -- too verbose with the many Rails versions

--- examples/example_helper.rb
+++ examples/example_helper.rb
@@ -39,12 +39,7 @@ def not_in_editor?
   ['TM_MODE', 'EMACS', 'VIM'].all? { |k| !ENV.has_key?(k) }
 end
 
-def in_runcoderun?
-  ENV["RUN_CODE_RUN"]
-end
-
 Micronaut.configure do |c|
-  c.formatter = :documentation if in_runcoderun?
   c.alias_example_to :fit, :focused => true
   c.alias_example_to :xit, :disabled => true
   c.mock_with :mocha
